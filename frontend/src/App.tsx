f0a7ae3dae7b9febfb0ad83c586fa53bb69571a8
Hemanth1310 <hemanth13k@gmail.com>
2026-03-12 13:40:15 +0100

    frontend-header-set

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
 import './App.css'
-import { BrowserRouter } from 'react-router'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import Sidebar from './components/Layout/Sidebar'
+import Home from './views/Home'
 
 function App() {
 
   return (
     <BrowserRouter>
-      <div className='min-w-screen min-h-screen flex bg-gray-100'>
+      <div className='min-w-screen min-h-screen md:flex bg-gray-100 ' >
         <Sidebar/>
+        <div className="mt-24 md:mt-0 md:ml-24 md:w-full h-full">
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+          </Routes>
+        </div>
+       
       </div>
     </BrowserRouter>
   )
