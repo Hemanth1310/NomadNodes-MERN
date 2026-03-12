50ec59eba18c318eb61711f2c989feaa22ebbe74
Hemanth1310 <hemanth13k@gmail.com>
2026-03-12 13:40:30 +0100

    frontend-home-layout

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='w-full min-h-screen '>
+        Homefjklwejfklwjfklwjkl;jfklwjfklw
+    </div>
   )
 }
 
