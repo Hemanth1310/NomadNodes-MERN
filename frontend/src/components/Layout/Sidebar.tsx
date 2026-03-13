8c6a12d48d7a0f298a835c553fa1d81d4a5e43eb
Hemanth1310 <hemanth13k@gmail.com>
2026-03-13 13:04:17 +0100

    frintend-sidebar-final

--- frontend/src/components/Layout/Sidebar.tsx
+++ frontend/src/components/Layout/Sidebar.tsx
@@ -1,15 +1,43 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 import { ImageUp, MapPinned, Settings } from "lucide-react";
 
 const Sidebar = () => {
   const navigate = useNavigate();
-
   return (
-    <div className="w-full md:w-24  p-3 md:pt-10 h-24 md:min-h-screen absolute left-0 top-0 bg-gray-200 flex md:flex-col items-center gap-10 z-10 ">
-      <MapPinned className="p-3 rounded-2xl hover:bg-mist-100" size={58} color="#ff6a00" onClick={() => navigate("/")} />
-      <ImageUp className="p-3 rounded-2xl hover:bg-mist-100" size={58} color="#353131" onClick={() => navigate("/upload")} />
-      <Settings className="p-3 rounded-2xl hover:bg-mist-100" size={58} color="#353131" onClick={() => navigate("/settings")}/>
+    <div className="group w-22 hover:w-52 p-3 min-h-screen left-0 top-0 border-r border-r-mist-300 bg-mist-50 flex flex-col items-start gap-10 z-10 fixed transition-[width] duration-150 ease-in-out">
+      <span className="w-full flex items-center justify-start gap-3 p-3 rounded-2xl hover:bg-mist-200">
+        <MapPinned
+          className="shrink-0"
+          size={40}
+          color="#ff6a00"
+          onClick={() => navigate("/")}
+        />
+        <p className="whitespace-nowrap hidden opacity-0 translate-x-2 transition-all duration-150 group-hover:block group-hover:opacity-200 group-hover:translate-x-0">
+          NomadNodes
+        </p>
+      </span>
+      <span className="w-full flex items-center justify-start gap-3 p-3 rounded-2xl hover:bg-mist-200">
+        <ImageUp
+          className="shrink-0"
+          size={40}
+          color="#353131"
+          onClick={() => navigate("/")}
+        />
+        <p className="whitespace-nowrap hidden opacity-0 translate-x-2 transition-all duration-150 group-hover:block group-hover:opacity-200 group-hover:translate-x-0">
+          Upload
+        </p>
+      </span>
+      <span className="w-full flex items-center justify-start gap-3 p-3 rounded-2xl hover:bg-mist-200">
+        <Settings
+          className="shrink-0"
+          size={40}
+          color="#353131"
+          onClick={() => navigate("/")}
+        />
+        <p className="whitespace-nowrap hidden opacity-0 translate-x-2 transition-all duration-150 group-hover:block group-hover:opacity-200 group-hover:translate-x-0">
+          Settings
+        </p>
+      </span>
     </div>
   );
 };
