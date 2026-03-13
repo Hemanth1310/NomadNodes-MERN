503e69786ecb50da1866864999197d15fe89a580
Hemanth1310 <hemanth13k@gmail.com>
2026-03-13 15:40:44 +0100

    frontend-layout-fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,10 +8,10 @@ function App() {
 
   return (
     <BrowserRouter>
-      <div className='min-w-screen md:flex bg-mist-50 ' >
+      <div className='min-w-screen min-h-screen md:flex bg-mist-50 ' >
         <Sidebar/>
         <Header/>
-        <div className="mt-22 ml-22 md:w-full box-border overflow-y-scroll">
+        <div className="mt-22 ml-22 md:w-full box-border overflow-y-scroll p-3">
           <Routes>
             <Route path='/' element={<Home/>}/>
           </Routes>
