8b2986083a624ac2c1d349fc47a30e03ee0cda8b
Hemanth1310 <hemanth13k@gmail.com>
2026-03-16 14:23:54 +0100

    backend-protected-routes-userdetails

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
 import "dotenv/config";
 import express from "express";
 import authRouter from './AuthRoutes'
+import protectedRoutes from './protectedRoutes'
+import authenticateToken from "./Middleware/authMiddleware";
 const app = express();
 
 app.use('/api/auth/', authRouter )
+app.use('/api/protectedRoutes/',authenticateToken, protectedRoutes)
 
 const port = Number(process.env["PORT"]) || 3001;
 
---
+++ backend/src/protectedRoutes.ts
@@ -0,0 +1,46 @@
+import express from 'express'
+import { prisma } from './prisma'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
+
+const router  = express.Router()
+router.use(express.json())
+
+router.post('/userDetails',async(req,res)=>{
+    const userId = req.user?.userId
+
+    try{
+        const user = await prisma.user.findFirst({
+            where:{
+                id:userId
+            },
+           select:{
+                id:true,
+                email:true,
+                password:true,
+                firstName:true,
+                lastName:true,
+                imagePath:true,
+                isVerified:true,
+            }
+        })
+
+        if(!user){
+            return res.status(404).json({errorMessage:"User not found"})
+        }
+        return res.status(200).json({
+            message:'Login Successful',
+            payload:{...user}
+        })
+    }catch(error){
+          if(error instanceof PrismaClientKnownRequestError){
+                    if(error.code === '2025'){
+                        return res.status(401).json({errorMessage:'User not found'})
+                    }
+                }
+            console.error("Unexpected Error:", error);
+            return res.status(500).json({ errorMessage: "Internal server error" });
+        
+    }
+})
+
+export default router
