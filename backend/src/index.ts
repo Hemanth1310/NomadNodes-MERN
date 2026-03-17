1bf4eae74dc32918ee3c96949e5129446018e8f4
Hemanth1310 <hemanth13k@gmail.com>
2026-03-17 11:36:24 +0100

    backend-cors-update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,22 @@ import express from "express";
 import authRouter from './AuthRoutes'
 import protectedRoutes from './protectedRoutes'
 import authenticateToken from "./Middleware/authMiddleware";
+import cors from 'cors'
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 
 app.use('/api/auth/', authRouter )
 app.use('/api/protectedRoutes/',authenticateToken, protectedRoutes)
@@ -13,3 +28,4 @@ const port = Number(process.env["PORT"]) || 3001;
 app.listen(port, () => {
 	console.log(`Backend running on port ${port}`);
 });
+
