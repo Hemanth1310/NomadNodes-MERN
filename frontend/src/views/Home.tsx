6b9579e1a51f21fb98e2a9d976c9cc852b0124d2
Hemanth1310 <hemanth13k@gmail.com>
2026-03-19 13:50:04 +0100

    frontend-home-no-data-fall-back

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -29,32 +29,23 @@ const Home = () => {
     if(isError){
         return <Error errorMessage={error.message} refetch={refetch}/>
     }
+
   return (
     <div className='w-full pt-5 sm:pl-10 sm:pr-10'>
         {/* {Array.from(Array(26)).map(()=><div className='h-10 bg-amber-600'></div>)} */}
        <h1 className='text-2xl sm:text-4xl mb-5 sm:mb-10'>Explore the best of Nomad Community</h1>
        <div className='grid grid-cols-2 sm:grid-cols-4 gap-5'>
+        {filteredNodes.length>0?<>
             {filteredNodes?.map(node=><div className='flex flex-col gap-2' key={node.id}>
                 <img className='h-full rounded-2xl' src={getImageUrl(node.imageUrl)}/>
                 <div className='ml-2'>
                     <p className='font-medium'>{node.title}</p>
                     <p className='text-sm'>Tags: {node.tags.map((tag)=><span className='border rounded-2xl pr-1 pl-1 mr-1 text-xs'>{tag}</span>)}</p>
                 </div>
-            </div>)}
-            {data?.map(node=><div className='flex flex-col gap-2' key={node.id}>
-                <img className='h-full rounded-2xl' src={getImageUrl(node.imageUrl)}/>
-                <div className='ml-2'>
-                    <p className='font-medium'>{node.title}</p>
-                    <p className='text-sm'>Tags: {node.tags.map((tag)=><span className='border rounded-2xl pr-1 pl-1 mr-1 text-xs'>{tag}</span>)}</p>
-                </div>
-            </div>)}
-            {data?.map(node=><div className='flex flex-col gap-2' key={node.id}>
-                <img className='h-full rounded-2xl' src={getImageUrl(node.imageUrl)}/>
-                <div className='ml-2'>
-                    <p className='font-medium'>{node.title}</p>
-                    <p className='text-sm'>Tags: {node.tags.map((tag)=><span className='border rounded-2xl pr-1 pl-1 mr-1 text-xs'>{tag}</span>)}</p>
-                </div>
-            </div>)}
+            </div>)}</>
+            :<div className='text-2xl'>
+                No results found
+            </div>}
           
         </div>
     </div>
