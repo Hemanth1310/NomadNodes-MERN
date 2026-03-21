d4dee999bce0bd999f7f752238aa34cf97b40770
Hemanth1310 <hemanth13k@gmail.com>
2026-03-21 16:37:57 +0100

    frontend-data-query-baseurl-update

--- frontend/src/lib/dataQueryHooks.ts
+++ frontend/src/lib/dataQueryHooks.ts
@@ -2,10 +2,8 @@ import { useQuery } from "@tanstack/react-query"
 import api from "./axiosConfig"
 import type { Nodes } from "@/types"
 
-const baseUrl  = import.meta.env.VITE_API_URL
-
 const fetchAllNodes = async(): Promise<Nodes[]>  => {
-    const {data} =await api.get(`${baseUrl}/api/publucRoutes/allNodes`)
+    const {data} =await api.get('/api/publucRoutes/allNodes')
     return data.payload
 }
 
